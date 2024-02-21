d0d21b6a9859bbc0c067a8143a4e29938ee169c6
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-21 19:14:45 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/1920. Build Array from Permutation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1920.Build_Array_from_Permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
