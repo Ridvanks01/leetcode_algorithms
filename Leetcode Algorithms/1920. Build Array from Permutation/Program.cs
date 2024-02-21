3683f4ee9e0755b10f4e467045c603ab8c587f9a
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-21 19:46:24 +0300

    Algorithm solved

--- Leetcode Algorithms/1920. Build Array from Permutation/Program.cs
+++ Leetcode Algorithms/1920. Build Array from Permutation/Program.cs
@@ -8,8 +8,17 @@ namespace _1920.Build_Array_from_Permutation
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public int[] BuildArray(int[] nums)
         {
+            int[] newArray = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArray[i] = nums[nums[i]];
+            }
+
+            return newArray;
+        
         }
     }
 }
