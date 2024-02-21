dc107a772521155f3aa4dcc73799c36e76c90fc3
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-21 18:57:11 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/1929. Concatenation of Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1929.Concatenation_of_Array
+{
+    internal class Program
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] newArray = new int[nums.Length * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArray[i] = nums[i];
+                newArray[i + nums.Length] = nums[i];
+            }
+
+            return newArray;
+
+        }
+    }
+}
