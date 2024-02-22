8628513f08ba60739d257d8bf0bd55c7acffd639
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-22 19:20:50 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/1512. Number of Good Pairs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1512.Number_of_Good_Pairs
+{
+    internal class Program
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int goodPairs = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        goodPairs++;
+                    }
+                }
+            }
+
+            return goodPairs;
+        }
+    }
+}
