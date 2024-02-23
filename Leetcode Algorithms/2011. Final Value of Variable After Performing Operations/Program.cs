210afeccb21b6d69e36716531de66c7f5926a05d
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-23 17:55:29 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/2011. Final Value of Variable After Performing Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2011.Final_Value_of_Variable_After_Performing_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Leetcode Algorithms/2769. Find the Maximum Achievable Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2769.Find_the_Maximum_Achievable_Number
+{
+    internal class Program
+    {
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return num + t * 2;
+        }
+    }
+}
