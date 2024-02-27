3af763dcc08055d2dea6d13f72a476c9084c9ac3
Ridvanks01 <karasubasiridvan@gmail.com>
2024-02-27 20:59:14 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/217. Contains Duplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _217.Contains_Duplicate
+{
+    internal class Program
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numbers.Add(nums[i]);
+            }
+
+            if(nums.Length != numbers.Count)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
