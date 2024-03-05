cbea6b7d81c4609546e75b3b58b08277a91c2bc8
Ridvanks01 <karasubasiridvan@gmail.com>
2024-03-05 20:09:14 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/9. Palindrome Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Palindrome_Number
+{
+    internal class Program
+    {
+        public bool IsPalindrome(int x)
+        {
+            string stringX = Convert.ToString(x);
+
+            string xRev = new string(stringX.Reverse().ToArray());
+
+            if(xRev == stringX)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
