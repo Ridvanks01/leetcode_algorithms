d812ce1c227d2a3f428621f532956893e16341ef
Ridvanks01 <karasubasiridvan@gmail.com>
2024-07-23 10:03:10 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/2418. Sort The People/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        int n = names.Length;
+
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            dict.Add(heights[i], names[i]);
+        }
+
+        Array.Sort(heights);
+        Array.Reverse(heights);
+
+        for (int i = 0; i < n; i++)
+        {
+            names[i] = Convert.ToString(dict[heights[i]]);
+        }
+
+        return names;
+    }
+}
