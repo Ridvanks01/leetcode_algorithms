6b805bf5948781514b4ba08f3b5a5458201d8edd
Ridvanks01 <karasubasiridvan@gmail.com>
2024-07-23 11:29:42 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/58. Length of Last Word/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+
+        string newS = s.Trim();
+
+        string[] stringArray = newS.Split();
+
+        string word = stringArray[stringArray.Length - 1];
+
+        return word.Length;
+    }
+}
