29cce9fbefcf32d99261a5f710aac5097b0a1513
Ridvanks01 <karasubasiridvan@gmail.com>
2024-07-25 15:26:50 +0300

    Algorithm solved

---
+++ Leetcode Algorithms/27. Remove Element/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        int lastIndex = nums.Length - 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                while (nums[lastIndex] == val)
+                {
+                    lastIndex--;
+                }
+
+                nums[i] = nums[lastIndex];
+
+                lastIndex--;
+
+                k++;
+            }
+        }
+
+        return nums.Length - k;
+
+    }
+}
